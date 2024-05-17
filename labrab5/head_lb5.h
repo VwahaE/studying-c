63ee3873c5ad05d9e4a3624015a9a4ddd0b1c8bb
VwahaE <160425261+VwahaE@users.noreply.github.com>
2024-05-17 21:56:11 +0300

    Update head_lb5.h
    update task 1

--- labrab5/head_lb5.h
+++ labrab5/head_lb5.h
@@ -1,7 +1,45 @@
 // Начало header guard.
 #ifndef HEAD_LB5_H
 #define HEAD_LB5_H
+/* Заголовочный файл для работы с функциями операционной системы Windows */
+#include <Windows.h>
+
+/* Подключаем библиотеку для ввода/вывода */
 #include <iostream>
+
+/* Перечисление цвета (константу) в качестве индекса*/
+enum ConsoleColor
+{
+    Black         = 0,
+    Blue          = 1,
+    Green         = 2,
+    Cyan          = 3,
+    Red           = 4,
+    Magenta       = 5,
+    Brown         = 6,
+    LightGray     = 7,
+    DarkGray      = 8,
+    LightBlue     = 9,
+    LightGreen    = 10,
+    LightCyan     = 11,
+    LightRed      = 12,
+    LightMagenta  = 13,
+    Yellow        = 14,
+    White         = 15
+};
+
+enum Level{
+    ZERO = 0,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN
+};
+
+/* используем перечислитель для определения животных */
 
 enum Animal{
     pig,
@@ -11,6 +49,9 @@ enum Animal{
     dog,
     fish
 };
+
+// Прототип функции вывода задания 1
+void outputTaskOne();
 
 // Прототип функции разделителя заданий
 //void funcDesigner(bool valFl = 1);
